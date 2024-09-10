e5c4df62ffe02f8630536f9f42534421f5a5d929
Roberto Pusiol <rpusiol@hotmail.com>
2024-09-10 18:05:52 -0300

    ping

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3,6 +3,6 @@ import { ping } from '../controllers/indexController.js';
 
 const router = Router();
 
-router.get('/ping', ping);
-
+// router.get('/ping', ping);
+router.get('/', ping);
 export default router;

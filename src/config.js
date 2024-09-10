902a3f5b6b5bb87ee7b335ef841e9679b49fb938
Roberto Pusiol <rpusiol@hotmail.com>
2024-09-10 17:55:42 -0300

    envvar

--- src/config.js
+++ src/config.js
@@ -4,9 +4,14 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST= process.env.DB_HOST;
-export const DB_USER= process.env.DB_USER;
-export const DB_PASS= process.env.DB_PASS;
-export const DB_PORT= process.env.DB_PORT;
-export const DB_NAME= process.env.DB_NAME;
+// export const DB_HOST= process.env.DB_HOST;
+// export const DB_USER= process.env.DB_USER;
+// export const DB_PASS= process.env.DB_PASS;
+// export const DB_PORT= process.env.DB_PORT;
+// export const DB_NAME= process.env.DB_NAME;
 
+export const MYSQLHOST= process.env.MYSQLHOST;
+export const MYSQLUSER= process.env.MYSQLUSER;
+export const MYSQLPASSWORD= process.env.MYSQLPASSWORD;
+export const MYSQLPORT= process.env.MYSQLPORT;
+export const MYSQLDATABASE= process.env.MYSQLDATABASE;
--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ import { createPool } from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config({path: "./config.env"});
 
-import { DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME } from './config.js';
+import {MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLPORT, MYSQLDATABASE } from './config.js';
 
 export const pool = createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  port: DB_PORT,
-  database: DB_NAME,
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  port: MYSQLPORT,
+  database: MYSQLDATABASE,
 })

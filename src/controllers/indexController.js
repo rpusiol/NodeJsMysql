ff574159616e2e10a2d0b503449e1a39cfb8d5c0
Roberto Pusiol <rpusiol@hotmail.com>
2024-09-10 18:00:09 -0300

    envvar

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,6 +1,6 @@
 import { pool } from "../db.js";
 
 export const ping = async(req, res) => {
-  const [result] = await pool.query('select name from employee');
+  const [result] = await pool.query("select 'PONG' as result;");
   res.json(result);
 }
